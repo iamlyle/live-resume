8f2c04f0a1a2a985b3843569dcda95759f880441
Le Cong Ly <ly.le-cong@internal.banvien.com>
2022-04-29 17:51:24 +0700

    add file for LCL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,11 +7,11 @@ export const environment = {
   // replace the data below with your personal data
   personal: {
     caricature: "gbastos-illustration@2x-mask.png",
-    name: "Guilherme Borges Bastos",
-    birth: "1993-06-29",
-    email: "guilhermeborgesbastos@gmail.com",
-    phone: "+31 68 733 3020",
-    location: "Rotterdam, South Holland, Netherlands"
+    name: "Le Cong Ly",
+    birth: "15 Nov, 1998",
+    email: "lecongly@gmail.com",
+    phone: "0345935656",
+    location: "Thu Duc City, Ho Chi Minh, Viet Nam"
   },
   // replace the dummy data below with the real firebase configs
   firebaseConfig: {
